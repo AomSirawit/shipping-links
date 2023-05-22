a6338875a903f87aca2c43d1014c60d27ac01835
AomSirawit <makdeesirawit@gmail.com>
2023-05-23 05:07:43 +0700

    deploy
    deploy

---
+++ script.js
@@ -0,0 +1,7 @@
+let mainNav = document.getElementById("js-menu");
+let NavToggle = document.getElementById("js-nav-toggle");
+
+NavToggle.addEventListener("click", function() {
+    mainNav.classList.toggle('active');
+
+});
